354e5d7b13ca30da5099cd8f6ee739d54706b38c
Ifaz Ahmed Aflan <ifaz.mobile.dev@gmail.com>
2018-04-16 23:40:43 +0600

    From saidul PC, Permission, 16 April, top_up

--- app/src/main/java/com/mobisheba/aflan/mobisheba/MainActivity.java
+++ app/src/main/java/com/mobisheba/aflan/mobisheba/MainActivity.java
@@ -1,26 +1,19 @@
 package com.mobisheba.aflan.mobisheba;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
-
+    Button rechargeButton;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -51,10 +44,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        rechargeButton = findViewById(R.id.recharge);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        rechargeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(MainActivity.this, RechargeActivity.class);
+                MainActivity.this.startActivity(myIntent);
+            }
+        });
     }
 
 
---
+++ app/src/main/java/com/mobisheba/aflan/mobisheba/RechargeActivity.java
@@ -0,0 +1,13 @@
+package com.mobisheba.aflan.mobisheba;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class RechargeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recharge);
+    }
+
+}
