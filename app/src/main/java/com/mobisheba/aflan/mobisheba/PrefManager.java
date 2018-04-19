dd8976d013592d30118a9517871371edf8193967
Ifaz Ahmed Aflan <ifaz.mobile.dev@gmail.com>
2018-04-20 01:30:41 +0600

    from my PC 20-04-2018

---
+++ app/src/main/java/com/mobisheba/aflan/mobisheba/PrefManager.java
@@ -0,0 +1,38 @@
+package com.mobisheba.aflan.mobisheba;
+
+/**
+ * Created by Aflan on 4/19/2018.
+ */
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Lincoln on 05/05/16.
+ */
+public class PrefManager {
+    // Shared preferences file name
+    private static final String PREF_NAME = "androidhive-welcome";
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+}
